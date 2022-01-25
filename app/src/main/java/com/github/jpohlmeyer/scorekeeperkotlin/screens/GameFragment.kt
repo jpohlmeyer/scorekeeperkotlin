dcb95fb1260765fb3f1e2c10f357499b9a09b22e
jpohlmeyer <jpohlmeyer@techfak.uni-bielefeld.de>
2022-01-25 11:39:39 +0100

    Implement simple game view

--- app/src/main/java/com/github/jpohlmeyer/scorekeeperkotlin/screens/GameFragment.kt
+++ app/src/main/java/com/github/jpohlmeyer/scorekeeperkotlin/screens/GameFragment.kt
@@ -5,13 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.github.jpohlmeyer.scorekeeperkotlin.R
 import com.github.jpohlmeyer.scorekeeperkotlin.databinding.FragmentGameBinding
 import com.github.jpohlmeyer.scorekeeperkotlin.screens.game.GameViewModel
+import com.google.android.material.divider.MaterialDivider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -33,19 +34,31 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         populateTableWithPlayerNames()
+        binding.endButton.setOnClickListener(this::endGameButtonOnClick)
     }
 
-    fun populateTableWithPlayerNames() {
-        // TODO items as XML?
+    private fun populateTableWithPlayerNames() {
         viewModel.simpleGame().playerList.forEach { player ->
-            val tableRow = layoutInflater.inflate(R.layout.simple_game_row, null)
-
+            val tableRow = layoutInflater.inflate(R.layout.simple_game_row, binding.pointTable, false)
             tableRow.findViewById<TextView>(R.id.name).text = player.name
-            tableRow.findViewById<TextView>(R.id.totalpoints).text = player.points.toString()
-            tableRow.findViewById<TextView>(R.id.addpoints).text = ""
-
+            val totalPoints: TextView = tableRow.findViewById(R.id.totalpoints)
+            totalPoints.text = player.points.toString()
+            val newPoints: EditText = tableRow.findViewById(R.id.addpoints)
+            tableRow.findViewById<ImageButton>(R.id.add_points_button).setOnClickListener {
+                if (newPoints.text.isNotEmpty()) {
+                    // TODO do this with viewmodel
+                    totalPoints.text =
+                        ((totalPoints.text.toString()).toInt() + (newPoints.text.toString()).toInt()).toString()
+                    newPoints.setText("")
+                }
+            }
             binding.pointTable.addView(tableRow)
         }
+    }
+
+    private fun endGameButtonOnClick(view: View) {
+        val action = GameFragmentDirections.actionGameFragmentToStartFragment()
+        findNavController().navigate(action)
     }
 
     override fun onDestroyView() {
